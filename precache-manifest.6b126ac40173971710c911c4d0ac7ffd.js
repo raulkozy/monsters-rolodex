08f2d70267cc012f4b14a4ea8dfdb54b72f58e02
rahul.kohli@accionlabs.com <rahul.kohli@accionlabs.com>
2019-11-11 18:25:10 +0530

    Updates

---
+++ precache-manifest.6b126ac40173971710c911c4d0ac7ffd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0828d35991da1a6f2a2ebe9c38e632bc",
+    "url": "/rahulkohli23/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "982a9d0238fcc3c8defd",
+    "url": "/rahulkohli23/monsters-rolodex/static/css/main.34aa63dc.chunk.css"
+  },
+  {
+    "revision": "a20f2964f1e7103d046d",
+    "url": "/rahulkohli23/monsters-rolodex/static/js/2.3860c490.chunk.js"
+  },
+  {
+    "revision": "982a9d0238fcc3c8defd",
+    "url": "/rahulkohli23/monsters-rolodex/static/js/main.ac3df945.chunk.js"
+  },
+  {
+    "revision": "a3b00b495ef301543eaa",
+    "url": "/rahulkohli23/monsters-rolodex/static/js/runtime-main.3b3bfba4.js"
+  }
+]);
